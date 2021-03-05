90006c1527f57a32585a5233047a84ec588d65db
xxxl <xxxl@localhost>
2021-03-06 00:34:22 +0800

    change "Startup file" which generated by cubeide

--- Inc/System_Init.h
+++ Inc/System_Init.h
@@ -10,7 +10,10 @@
 
 void SystemInit(void);
 void SystemClock_Config(void);
+void AFIO_Config(void);
 void NVIC_Config(void);
-
+void delay_init(void);
+void delay_us(u32 nus);
+void delay_ms(u16 nms);
 
 #endif /* SYSTEM_INIT_H_ */
